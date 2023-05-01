1df6604f0cd9f87ddb997cd09293cdfe64e350cf
Helbul <bulina.nn@gmail.com>
2023-05-01 15:54:29 +0300

    lesson 5

--- app/src/main/java/com/example/materialappfromgb/view/picture/PictureFragment.kt
+++ app/src/main/java/com/example/materialappfromgb/view/picture/PictureFragment.kt
@@ -4,10 +4,14 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.view.*
+import android.widget.ImageView
 import android.widget.Toast
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.coordinatorlayout.widget.CoordinatorLayout
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import androidx.transition.*
 import coil.load
 import com.example.materialappfromgb.MainActivity
 import com.example.materialappfromgb.R
@@ -43,6 +47,8 @@ class PictureFragment : Fragment() {
         ViewModelProvider(this).get(PictureOfTheDayViewModel::class.java)
     }
 
+    var isFlag = false
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel.getLiveData().observe(viewLifecycleOwner
@@ -51,6 +57,31 @@ class PictureFragment : Fragment() {
         }
 
         viewModel.sendRequest()
+
+        binding.imageView.setOnClickListener() {
+            isFlag = !isFlag
+
+            val params = it.layoutParams as ConstraintLayout.LayoutParams
+
+            val transitionSet = TransitionSet()
+            val changeImageTransform = ChangeImageTransform()
+            val changeBounds = ChangeBounds()
+            changeBounds.duration = 2000L
+            changeImageTransform.duration = 2000L
+
+            transitionSet.addTransition(changeBounds)
+            transitionSet.addTransition(changeImageTransform)
+
+            TransitionManager.beginDelayedTransition(binding.root, transitionSet)
+            if (isFlag) {
+                params.height = ConstraintLayout.LayoutParams.MATCH_PARENT
+                binding.imageView.scaleType = ImageView.ScaleType.CENTER_CROP
+            } else {
+                params.height = CoordinatorLayout.LayoutParams.WRAP_CONTENT
+                binding.imageView.scaleType = ImageView.ScaleType.CENTER_INSIDE
+            }
+            binding.imageView.layoutParams = params
+        }
     }
 
     private fun renderData (appState: AppState) {
@@ -58,9 +89,21 @@ class PictureFragment : Fragment() {
             is AppState.Error -> TODO()
             AppState.Loading -> Toast.makeText(context, "Loading", Toast.LENGTH_SHORT).show()
             is AppState.Success -> {
+                val myAutoTransaction = TransitionSet()
+                myAutoTransaction.ordering = TransitionSet.ORDERING_SEQUENTIAL
+                val slide = Slide(Gravity.END)
+                slide.duration = 1000L
+                val changeBounds = ChangeBounds()
+                changeBounds.duration = 2000L
+                myAutoTransaction.addTransition(changeBounds)
+                myAutoTransaction.addTransition(slide)
+                TransitionManager.beginDelayedTransition(binding.root, myAutoTransaction)
+                binding.imageView.visibility = ImageView.VISIBLE
                 binding.imageView.load(appState.pictureOfTheDayResponseData.url)
                 //Нстроить загрузку изображения erorr() placeholder()
+                binding.textviewFirst.visibility = TextureView.VISIBLE
                 binding.textviewFirst.text = appState.pictureOfTheDayResponseData.title
+
             }
         }
     }
